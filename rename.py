51eac1ee2d5f1605bc298545027307ab9d731bee
danx0r <danbmil99@gmail.com>
2014-07-10 13:25:38 -0700

    rename

--- rename.py
+++ rename.py
@@ -3,12 +3,26 @@ import os, sys
 new = sys.argv[1]
 
 def do(path):
+    oldpath = os.path.abspath('.')
+    os.chdir(path)
+#     print "-->", path
     for f in os.listdir(path):
+#         print "---->", f
         if f[:4] == ".git" or f[-4:] == ".pyc" or f == __file__:
             continue
         f = os.path.abspath(f)
-        print f
         if os.path.isdir(f):
             do(f)
-
+        else:
+            print f
+            g = open(f)
+            s = g.read()
+            g.close()
+            s = s.replace("djangsimple", new)
+            g = g.open(f, 'w')
+#             g.write(s)
+            print s
+            g.close()
+            
+    os.chdir(oldpath)
 do('.')
