62632e1f30bc7ff1329f1a8dc29672c72019b5a2
danx0r <dan.miller@hiqlabs.com>
2018-07-09 16:27:44 -0700

    djangle new thoughts

--- example.py
+++ example.py
@@ -1,33 +1,17 @@
-import djangle as dj
-import json
+#this file gets imported by djangle
+import djhelpers as dj
 
 MONGO="mongodb://127.0.0.1:27017"
-
 dj.mongo_set(MONGO, "test_db")
 
-def get_version():
+#
+# Create endpoint /version
+#
+def version():
     return ("0.0.001")
 
-# def check_upsert(data):
-#     j=json.loads(data)
-#     k = j.keys()
-#     if len(k) != 2:
-#         error()
-#     if 'data' not in k:
-#         error()
-#     if 'type' not in k:
-#         error()
-#     dj.mongo_write(data)
-# schema check:
 #
-# dj.create_endpoint("api",
-#                 post=True,
-#                 options={'upsert':dj.check_upsert,
-#                          'getVersion':get_version,
-#                          'search':dj.mongo_read})
-
-dj.create_endpoint("api",
-                get=True,
-                post=True,
-                options={'upsert':dj.mongo_write,
-                         'getVersion':get_version})
+# Create endpoint /api?search=abcdef123
+#
+def api(search):
+    return dj.mongo_query(search)
