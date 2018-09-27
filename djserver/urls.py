8a7ddb6dffc0ff05eceb8e50b9f4cae07e121350
danx0r <dan.miller@hiqlabs.com>
2018-09-26 17:48:30 -0700

    login page

--- djserver/urls.py
+++ djserver/urls.py
@@ -2,11 +2,14 @@ from django.conf.urls import url
 import djserver.views as views
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^login$', views.login, name='login'),
-    url(r'^logout$', views.logout, name='logout'),
+    url(r'^login/$', auth_views.LoginView.as_view(), name='login'),
+    url(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+    url(r'^api_login$', views.login, name='login'),
+    url(r'^api_logout$', views.logout, name='logout'),
     url(r'^whoami$', views.whoami, name='whoami'),
     url(r'^', views.home, name='home'),
 ]
