1a4ecbef0f955a0f8453bda593a85fe1b5b15461
danx0r <ubuntu@ip-172-31-19-194.us-west-2.compute.internal>
2018-10-18 23:28:41 +0000

    check for djongo in settings

--- djserver/settings.py
+++ djserver/settings.py
@@ -10,13 +10,20 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'djongo',
-        'NAME': 'djangle',
-        'HOST': 'localhost',
-        'PORT': 27017,
+try:
+    import djongo
+    DATABASES = {
+        'default': {
+            'ENGINE': 'djongo',
+            'NAME': 'djangle',
+            'HOST': 'localhost',
+            'PORT': 27017,
+        }
     }
+except:
+    DATABASES = {
+    }
+
     # 'default': {
     #     'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
     #     'NAME': '',                      # Or path to database file if using sqlite3.
@@ -26,7 +33,7 @@ DATABASES = {
     #     'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
     #     'PORT': '',                      # Set to empty string for default.
     # }
-}
+
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
