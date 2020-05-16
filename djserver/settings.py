f1e0341fa297fd595d8281652b9c63061858a4b1
danx0r <dan.miller@eye0.com>
2020-05-16 13:47:32 -0700

    serve static in view to avoid unnecessary complications

--- djserver/settings.py
+++ djserver/settings.py
@@ -66,33 +66,35 @@ MEDIA_ROOT = ''
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = ''
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+### handling static files in view.py
 
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
-
-staticpath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '')) + "/static/"
-print ("DXBUG file:", __file__, "static path:", staticpath)
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    staticpath,
-)
-
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
+# # Absolute path to the directory static files should be collected to.
+# # Don't put anything in this directory yourself; store your static files
+# # in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# # Example: "/var/www/example.com/static/"
+# STATIC_ROOT = ''
+#
+# # URL prefix for static files.
+# # Example: "http://example.com/static/", "http://static.example.com/"
+# STATIC_URL = '/static/'
+#
+# staticpath = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '')) + "/static/"
+# print ("DXBUG file:", __file__, "static path:", staticpath)
+# # Additional locations of static files
+# STATICFILES_DIRS = (
+#     # Put strings here, like "/home/html/static" or "C:/www/django/static".
+#     # Always use forward slashes, even on Windows.
+#     # Don't forget to use absolute paths, not relative paths.
+#     staticpath,
+# )
+#
+# # List of finder classes that know how to find static files in
+# # various locations.
+# STATICFILES_FINDERS = (
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+# )
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '#31@fa4j_(tgz^9y5r$!c(1h5s)jcepae5hp#co5nv&#ar%re6'
@@ -171,7 +173,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'corsheaders',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
